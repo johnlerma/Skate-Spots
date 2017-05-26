1ea9396c0dfb5731dca4aa77042242725530a440
johnlerma <johnlerma@gmail.com>
2017-05-26 12:06:52 -0700

    added filter
    Added filter
    filter does not link to google map markers yet
    removed unneeded code

--- js/app.js
+++ js/app.js
@@ -1,7 +1,9 @@
-var spots = [{
+var spots = ko.observableArray([{
         title: 'Wallenberg 4 Step',
+        name: 'bbbbbb',
         url: 'http://www.url.com',
         icon: 'http://maps.google.com/mapfiles/ms/icons/blue-dot.png',
+        type: 'stairs',
         location: {
             lat: 37.7802,
             lng: -122.446864
@@ -9,8 +11,10 @@ var spots = [{
     },
     {
         title: 'Clipper Hubba',
+         name: 'ccccc',
         url: 'http://www.url.com',
         icon: 'http://maps.google.com/mapfiles/ms/icons/blue-dot.png',
+        type: 'ledges',
         location: {
             lat: 37.749408,
             lng: -122.43222
@@ -20,6 +24,7 @@ var spots = [{
         title: 'Sega Circle Ledges',
         url: 'http://www.url.com',
         icon: 'http://maps.google.com/mapfiles/ms/icons/blue-dot.png',
+        type: 'skatepark',
         location: {
             lat: 37.769155,
             lng: -122.405325
@@ -29,6 +34,7 @@ var spots = [{
         title: 'Pier 7 Stair Set',
         url: 'http://www.url.com',
         icon: 'http://maps.google.com/mapfiles/ms/icons/blue-dot.png',
+        type: 'ledges',
         location: {
             lat: 37.798694,
             lng: -122.396752
@@ -38,63 +44,72 @@ var spots = [{
         title: 'Fort Miley Pyramids',
         url: 'http://www.url.com',
         icon: 'http://maps.google.com/mapfiles/ms/icons/blue-dot.png',
+        type: 'bank',
         location: {
             lat: 37.783255,
             lng: -122.508833
         }
     }
-];
+]);
 
-var Spot = function(data) {
-    var self = this;
-    this.title = ko.observable(data.title);
-    this.location = ko.observable(data.location);
-};
+
+
+ var planets = ko.observableArray([
+        { name: "Mercury", type: "rock"},
+        { name: "Venus", type: "rock"},
+        { name: "Earth", type: "rock"},
+        { name: "Mars", type: "rock"},
+        { name: "Jupiter", type: "gasgiant"},
+        { name: "Saturn", type: "gasgiant"},
+        { name: "Uranus", type: "gasgiant"},
+        { name: "Neptune", type: "gasgiant"}
+    ]);
 
 var ViewModel = function() {
     var self = this;
+    this.displayMessage = ko.observable(true);
     this.spotList = ko.observableArray([]);
-    spots.forEach(function(spotItem) {
-        self.spotList.push(new Spot(spotItem));
-    });
+
+    
+    this.typeToShow = ko.observable("all");
+
+    this.displayAdvancedOptions = ko.observable(false);
+    
+     this.addPlanet = function(type) {
+        this.planets.push({
+            name: "New planet",
+            type: type
+        });
+    };
+    
+    this.planetsToShow = ko.pureComputed(function() {
+        // Represents a filtered list of planets
+        // i.e., only those matching the "typeToShow" condition
+        var desiredType = this.typeToShow();
+        if (desiredType == "all") return spots();
+        return ko.utils.arrayFilter(spots(), function(spot) {
+            return spot.type == desiredType;
+        });
+    }, this);
+    
+     // Animation callbacks for the planets list
+    this.showPlanetElement = function(elem) { if (elem.nodeType === 1) $(elem).hide().slideDown() }
+    this.hidePlanetElement = function(elem) { if (elem.nodeType === 1) $(elem).slideUp(function() { $(elem).remove(); }) }
+    
 };
 
-var locations = ko.observableArray([{
-        title: 'Wallenberg 4 Step',
-        location: {
-            lat: 37.7802,
-            lng: -122.446864
-        }
+ko.bindingHandlers.fadeVisible = {
+    init: function(element, valueAccessor) {
+        // Initially set the element to be instantly visible/hidden depending on the value
+        var value = valueAccessor();
+        $(element).toggle(ko.unwrap(value)); // Use "unwrapObservable" so we can handle values that may or may not be observable
     },
-    {
-        title: 'Clipper Hubba',
-        location: {
-            lat: 37.749408,
-            lng: -122.43222
-        }
-    },
-    {
-        title: 'Sega Circle Ledges',
-        location: {
-            lat: 37.769155,
-            lng: -122.405325
-        }
-    },
-    {
-        title: 'Pier 7 Stair Set',
-        location: {
-            lat: 37.798694,
-            lng: -122.396752
-        }
-    },
-    {
-        title: 'Fort Miley Pyramids',
-        location: {
-            lat: 37.783255,
-            lng: -122.508833
-        }
-    },
-]);
+    update: function(element, valueAccessor) {
+        // Whenever the value subsequently changes, slowly fade the element in or out
+        var value = valueAccessor();
+        ko.unwrap(value) ? $(element).fadeIn() : $(element).fadeOut();
+    }
+};
 
 
 // Function to initialize the map within the map div
@@ -113,10 +128,10 @@ function initMap() {
     var bounds = new google.maps.LatLngBounds();
 
 
-    for (var i = 0; i < spots.length; i++) {
-        var position = spots[i].location;
-        var title = spots[i].title;
-        var icon = spots[i].icon;
+    for (var i = 0; i < spots().length; i++) {
+        var position = spots()[i].location;
+        var title = spots()[i].title;
+        var icon = spots()[i].icon;
         var that = this;
         // Create a marker per location, and put into markers array.
         var marker = new google.maps.Marker({
@@ -131,7 +146,7 @@ function initMap() {
         markers.push(marker);
 
         // creates links on the left panel
-        $('#markers').append('<a class="marker-link" data-markerid="' + i + '" href="#">' + locations()[i].title + '</a><br><br>');
+//        $('#markers').append('<a class="marker-link" data-markerid="' + i + '" href="#">' + locations()[i].title + '</a><br><br>');
 
         // Create an onclick event to open an infowindow at each marker.
         marker.addListener('click', function() {
@@ -200,5 +215,7 @@ function initMap() {
 
 }
 //var createMap = initMap();
-
+//ko.applyBindings({
+//    displayMessage: ko.observable(true)
+//});
 ko.applyBindings(new ViewModel());
